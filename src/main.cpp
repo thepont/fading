0dafda33b7ab978c67bef47b2e9a97726c2ba905
Paul Esson <paul.esson@blueflag.com.au>
2016-06-09 20:05:37 +1000

    cleanup code

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,18 @@
 #include "StrobeLights.h"
 #include "ConstantLight.h"
 
-const int BUTTON_INPUT = 2;
 const int MODE_ARRAY_SIZE = 12;
+const int LED_AMOUNT = 3;
+const int UPPER_BOUNDS = MODE_ARRAY_SIZE - LED_AMOUNT;
+
+const int BUTTON_INPUT_PIN = 2;
+// How long to wait before switching modes
+const int BUTTON_READ_MS = 1000;
+
+// Pins that the LEDS are on
 const int LED_1_PIN = 9;
 const int LED_2_PIN = 10;
 const int LED_3_PIN = 11;
-
 
 FlickerLights fl1(LED_1_PIN, 255, 150 , 170, 0, 0, 10);
 FlickerLights fl2(LED_2_PIN, 160, 40, 170, 10, 6, 60);
@@ -35,44 +41,48 @@ int b1;
 void setup(){
   // Serial.begin(9600);
 
-  //Button Init.
-  pinMode(BUTTON_INPUT,INPUT);
-  digitalWrite(BUTTON_INPUT, HIGH);
+  //Button Init Pull Up
+  pinMode(BUTTON_INPUT_PIN,INPUT);
+  digitalWrite(BUTTON_INPUT_PIN, HIGH);
 
   //Populate Light Runners
+  //Flicker and 1 Constant
   lightRunnders[0] = &fl1;
   lightRunnders[1] = &fl2;
   lightRunnders[2] = &cl1;
 
+  //All Flicker
   lightRunnders[3] = &fl1;
   lightRunnders[4] = &fl2;
   lightRunnders[5] = &fl3;
 
+  //Constant On High
   lightRunnders[6] = &constantBright1;
   lightRunnders[7] = &constantBright2;
   lightRunnders[8] = &constantBright3;
 
+  //Strobe off beat pulse
   lightRunnders[9] = &sl1;
   lightRunnders[10] = &sl2;
   lightRunnders[11] = &sl3;
 }
 
 void loop()  {
-  lightRunnders[basePtr]->run();
-  lightRunnders[basePtr+1]->run();
-  lightRunnders[basePtr+2]->run();
+  for(int i = 0; i < LED_AMOUNT; i++){
+    lightRunnders[basePtr+i]->run();
+  }
 
-  b1 = digitalRead(BUTTON_INPUT);
+  b1 = digitalRead(BUTTON_INPUT_PIN);
 
   if(b1 == 0){
     if( b1 != buttonPrevious){
       buttonReadMillis = millis();
     }
     currentMillis = millis();
-    if( currentMillis - buttonReadMillis >= 1000 ){
+    if( currentMillis - buttonReadMillis >= BUTTON_READ_MS ){
       buttonReadMillis = currentMillis;
-      if(basePtr <= 9){
-         basePtr+=3;
+      if(basePtr <= UPPER_BOUNDS){
+         basePtr += LED_AMOUNT;
       }else{
         basePtr = 0;
       }
